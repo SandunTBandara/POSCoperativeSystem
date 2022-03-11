8b9a3bc1d7d4c6c43305f63cd5dbb238ffa165bc
Shadhani-Rushanthi <100184787+Shadhani-Rushanthi@users.noreply.github.com>
2022-03-11 12:19:28 +0530

    Update ManageItem.java

--- src/controller/ManageItem.java
+++ src/controller/ManageItem.java
@@ -83,7 +83,7 @@ public class ManageItem {
     {  ResultSet result=null;
        try{
          Connection con = DB.createConnection();
-         String sql = "select * from item where department = "+Integer.parseInt(dno)+" and category = '"+cno+"' ";
+         String sql = "select * from item where dno = "+Integer.parseInt(dno)+" and category = '"+cno+"' ";
          Statement stmt = con.createStatement();
          result = stmt.executeQuery(sql);
           
@@ -222,6 +222,84 @@ public class ManageItem {
       
     }
     
+    
+    public ResultSet getInvoiceByCode(String code){
+        ResultSet result=null;
+        try{
+            
+            Connection con = DB.createConnection();
+            String sql = "select * from customerinvoice where invoiceNo = '" + code + "';";
+            Statement stmt = con.createStatement();
+            result = stmt.executeQuery(sql);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
+    
+    public int saveItem(){
+        int result=0;
+        try{
+            Connection con = DB.createConnection();
+            String sql = "INSERT INTO item(code, itemName, sinhalaName, barcode, margin, weight, costPrice, sellingPrice, reorderLevel, reorderQty, maxQty, category, dno, Bcode, scode) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, Item.getItemCode());
+            stmt.setString(2, Item.getName());
+            stmt.setString(3, Item.getSname());
+            stmt.setInt(4, Item.getBarcode());
+            stmt.setDouble(5, Item.getMargin());
+            stmt.setString(6, Item.getWeight());
+            stmt.setDouble(7, Item.getCostType());
+            stmt.setDouble(8, Item.getSellingPrice());
+            stmt.setDouble(9, Item.getReorderLevel());
+            stmt.setDouble(10, Item.getReorderQty());
+            stmt.setDouble(11, Item.getMaxQty());
+            stmt.setString(12, Item.getCategory());
+            stmt.setInt(13, Item.getDepartment());
+            stmt.setInt(14, Item.getBinType());
+            stmt.setInt(15, Item.getSupplier());
+    
+            result = stmt.executeUpdate();
+        }catch(Exception e){
+            System.out.println("line 265" +e.getMessage());
+        }
+        return result;
+    }
+    
+    public void updateItem()
+    {        
+        try{
+             Connection con = DB.createConnection();
+             String sql = "UPDATE item SET itemName = ?, sinhalaName=?, barcode=?,margin = ?, weight=?, costPrice=?, sellingPrice =?, reorderLevel = ?, reorderQty =?, maxQty =?, category = ?, dno =?, Bcode =?, scode = ?   WHERE code = "+ Item.getItemCode() + ";";
+             PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, Item.getName());
+            stmt.setString(2, Item.getSname());
+            stmt.setInt(3, Item.getBarcode());
+            stmt.setDouble(4, Item.getMargin());
+            stmt.setString(5, Item.getWeight());
+            stmt.setDouble(6, Item.getCostType());
+            stmt.setDouble(7, Item.getSellingPrice());
+            stmt.setDouble(8, Item.getReorderLevel());
+            stmt.setDouble(9, Item.getReorderQty());
+            stmt.setDouble(10, Item.getMaxQty());
+            stmt.setString(11, Item.getCategory());
+            stmt.setInt(12, Item.getDepartment());
+            stmt.setInt(13, Item.getBinType());
+            stmt.setInt(14, Item.getSupplier());
+
+             int result = stmt.executeUpdate();
+
+             if (result > 0) {
+                 JOptionPane.showMessageDialog(null, "Item has been updated succesfully ", "SuperMarket Management", JOptionPane.INFORMATION_MESSAGE);
+             } 
+        }
+        catch(Exception e)
+        {
+          System.out.println(e.getMessage());
+        }
+    }
     
     
 }
