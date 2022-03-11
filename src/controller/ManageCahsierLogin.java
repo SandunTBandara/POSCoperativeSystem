4e062b8cc085a59c3106fc8554a4dc98a77b20e0
Shadhani-Rushanthi <100184787+Shadhani-Rushanthi@users.noreply.github.com>
2022-03-11 12:05:12 +0530

    Update ManageCahsierLogin.java

--- src/controller/ManageCahsierLogin.java
+++ src/controller/ManageCahsierLogin.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controller;
 
 import java.sql.Connection;
@@ -13,10 +9,6 @@ import javax.swing.JOptionPane;
 import model.DB;
 import model.CashierLogin;
 
-/**
- *
- * @author Sanu
- */
 public class ManageCahsierLogin {
     
     CashierLogin clogin1;
